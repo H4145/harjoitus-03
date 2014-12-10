c9d8e7315c47202730700087870f969894511c32
H4145 <ede.1994@gmail.com>
2014-12-10 13:06:27 +0200

    Harjoitus 3

---
+++ harj3a.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+void main(void)
+{
+	cout <<
+		"Eetu Salo \n"
+		"Ilmarisenkatu 2 D76";
+}
+		
---
+++ harj3b.cpp
@@ -0,0 +1,53 @@
+/*
+Eetu Salo
+
+Harjoitus 3 (palautus vko 39)
+
+#include <iostream>
+using namespace std;
+void main (void)
+{
+cout << "Hei maailma!";
+}
+
+Muokkaa ylla olevaa ohjelmaa niin, etta tulostat oman nimesi
+ja osoitteesi naytolle allekkain.
+HUOM! Nimeä tiedosto harj3a.cpp nimelle!
+
+Muokkaa edellisestä ohjelmasta versio nimelle harj3b.cpp, jossa
+nimi ja osoite tulostetaan näytälle seuraavasti:
+
+**********************************
+* *
+* Etunimi *
+* Sukunimi *
+* *
+*-------------------------------------- *
+* *
+* Asemakatu 7 *
+* 40520 JKL *
+* *
+**********************************
+
+*/
+
+
+
+#include <iostream>
+using namespace std;
+void main(void)
+{
+	cout <<
+		"********************************** \n"
+		"* * \n"
+		"* Eetu * \n"
+		"* Salo * \n"
+		"* * \n"
+		"*-------------------------------------- * \n"
+		"* * \n"
+		"* Ilmarisenkatu 2 D76 * \n"
+		"* 40100 JKL * \n"
+		"* * \n"
+		"**********************************";
+}
+		
